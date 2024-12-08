3b0422ee087886dcd3a39f8d4e6ffece7f9b23d7
owayo-cloud <owayopaul@gmail.com>
2024-12-08 13:34:06 +0300

    create_profiles_table

--- profiles-page/database/migrations/2024_12_08_103004_create_profiles_table.php
+++ profiles-page/database/migrations/2024_12_08_103004_create_profiles_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
